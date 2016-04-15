543f41a00c522e752ac3a7866532261d8f24b823
Robert Cunningham <rgc3dev@gmail.com>
2016-04-15 11:41:16 -0400

    updated class code

--- todo/todo.js
+++ todo/todo.js
@@ -1,35 +1,47 @@
 var tasks = [
-  {title: "Eat Panera with Zack", completed: true},
-  {title: "Walk the dog", completed: false},
-  {title: "Create a JavaScript App", completed: false}
+  {
+    title: "Eat Panera with Zack",
+    completed: true
+  },
+  {
+    title: "Walk the dog Scruffy",
+    completed: false
+  },
+  {
+    title: "Create a JavaScript app",
+    completed: false
+  }
 ];
 
-function addTask(newTask) {
-  tasks.push({title: newTask, completed: false});
+function addTask(taskThatImGoingToAdd) {
+  tasks.push({
+    title: taskThatImGoingToAdd,
+    completed: false
+  });
   updateView();
 }
 
-function toggleTaskCompleted() {
-  var completed = $(this).attr("completed");
-  completed = !completed
-  $(this).attr("completed", completed);
+function toggleTaskCompleted(el) {
+  var taskIndex = $(el).attr('taskId');
+  tasks[taskIndex].completed = !tasks[taskIndex].completed;
+  updateView();
 }
 
 function updateView() {
-// reference the "ul" element in our HTML
+  // reference the "ul" element in our HTML
   var taskListDiv = document.getElementById("taskList");
   taskListDiv.innerHTML = "";
-
-// for each task I need to create a "li" element
-// in each "li" element I want to display the task
-// then, I want to add the task to the task list
-  for (var i = 0; i < tasks.length; i++) {
+  // for each task, I need to create a "li" element
+  // in each "li" element, I want to display the task
+  // then, I want to add the task to the task list
+  tasks.forEach(function(task, index) {
     var individualTask = document.createElement("li");
-    individualTask.innerHTML = tasks[i].title;
-    individualTask.setAttribute("completed", tasks[i].completed);
-    individualTask.onclick = toggleTaskCompleted;
+    individualTask.innerHTML = task.title;
+    individualTask.setAttribute('completed', task.completed);
+    individualTask.setAttribute('taskId', index);
+    individualTask.setAttribute('onclick', "toggleTaskCompleted(this)");
     taskListDiv.appendChild(individualTask);
-  }
+  });
 }
 
 updateView();
