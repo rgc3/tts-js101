d6a65811e3d4aea5281f5c3ab0ee03b7bdb7449c
Robert Cunningham <rgc3dev@gmail.com>
2016-04-26 08:44:16 -0400

    Add todo2. Set up firebase. Add Bootstrap CDN for FontAwesome.

--- todo2/todo2.js
+++ todo2/todo2.js
@@ -1,35 +1,36 @@
-var tasks = [
-  {
-    title: "Eat Panera with Zack",
-    completed: true
-  },
-  {
-    title: "Walk the dog Scruffy",
-    completed: false
-  },
-  {
-    title: "Create a JavaScript app",
-    completed: false
-  }
-];
+var firebaseReference = new Firebase("https://shining-heat-2879.firebaseio.com/tasks/");
+
+
+var tasks = [];
+firebaseReference.on("value", function(value){
+  tasks = value.val();
+  updateView();
+});
 
 function addTask(taskThatImGoingToAdd) {
+  // add new tasks to tasks array
   tasks.push({
     title: taskThatImGoingToAdd,
     completed: false
   });
+  // clear out the input box
+  document.getElementById("newTask").value = "";
+  firebaseReference.set(tasks);
+  // replacing the old tasks array in firebase with our new tasks array in our todo app
   updateView();
 }
 
 function toggleTaskCompleted(el) {
   var taskIndex = $(el).attr('taskId');
   tasks[taskIndex].completed = !tasks[taskIndex].completed;
+  firebaseReference.set(tasks);
   updateView();
 }
 
 function removeTask(el) {
   var taskIndex = $(el).attr('taskId');
   tasks.splice(taskIndex, 1);
+  firebaseReference.set(tasks);
   updateView();
 }
 
@@ -41,13 +42,18 @@ function updateView() {
   // in each "li" element, I want to display the task
   // then, I want to add the task to the task list
   tasks.forEach(function(task, index) {
-    var individualTask = document.createElement('li');
+    // creating an empty li element, assigning the value to individualTask
+    var individualTask = document.createElement('li'); // creates empty <li>
+    // taking the title property of each task, putting the text into the list item
+    // <li>task text</li>
     individualTask.innerHTML = task.title;
-    individualTask.setAttribute('completed', task.completed);
-    individualTask.setAttribute('taskId', index);
+    // as I'm iterating through each task, each task has a property called completed (boolean)
+    // <li completed="true">Task Text</li>
+    individualTask.setAttribute('completed', task.completed); //(name of attribute, value)
+    individualTask.setAttribute('taskId', index); //setting taskid = index
     individualTask.setAttribute('onclick', 'toggleTaskCompleted(this)');
     individualTask.setAttribute('ondblclick', 'removeTask(this)');
-    taskListDiv.appendChild(individualTask);
+    taskListDiv.appendChild(individualTask); // put individualTask inside of the taskListDiv
   });
   $('li').on('swipe', function(el) {
     var taskIndex = $(this).attr('taskId');
